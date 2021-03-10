4bf3cde5490a20d883204dcbb17947be4f4906a1
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-03-09 20:26:19 -0800

    added pages

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,31 +1,45 @@
   
-import React from 'react';
-import "./style.css";
-import {Link} from "react-router-dom";
+// import React from 'react';
+// import "./style.css";
+// import {Link} from "react-router-dom";
 
+
+// function Nav() {
+
+//     return (
+//         <div>
+//             <nav className="navbar navbar-expand-lg navbar-dark">
+//                 <Link className="navbar-brand" to="/">Google Books</Link>
+//                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                     <span className="navbar-toggler-icon"></span>
+//                 </button>
+//                 <div className="collapse navbar-collapse" id="navbarNav">
+//                     <ul className="navbar-nav">
+//                         <li className="nav-item active">
+//                             <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
+//                         </li>
+//                         <li className="nav-item active">
+//                             <Link className="nav-link" to="/saved">Saved</Link>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </nav>
+//         </div>
+//     )
+// }
+
+// export default Nav;
+
+import React from "react";
 
 function Nav() {
-
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark">
-                <Link className="navbar-brand" to="/">Google Books</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/saved">Saved</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        React Reading List
+      </a>
+    </nav>
+  );
 }
 
 export default Nav;
